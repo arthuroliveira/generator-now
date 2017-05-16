86d80d2bea0957519546ab09cfc6604d037ed89d
Arthur Oliveira <arthur.gmraes@gmail.com>
2017-05-16 16:58:04 -0700

    g runt file and package json

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,7 +13,7 @@ module.exports = class extends Generator {
     const prompts = [{
       type: 'confirm',
       name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      message: 'Would you like to enable this option???',
       default: true
     }];
 
@@ -24,13 +24,21 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    // this.fs.copyTpl(
+    //   this.templatePath('dummyfile.txt'),
+    //   this.destinationPath('dummyfile.txt'),
+    //   this.props
+    // );
+
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('**/*'),
+      // this.destinationRoot()
+      this.destinationPath('client'
     );
+
   }
 
   install() {
-    this.installDependencies();
+    // this.installDependencies();
   }
 };
